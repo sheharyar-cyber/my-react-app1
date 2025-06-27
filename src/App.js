efce99d48063402df22b93be7d3350a27b21d893
Sheharyar Ahmad <sherryjaisak@gmail.com>
2025-06-27 14:49:11 +0500

    Update: test CI/CD auto-deploy from GitHub Actions

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       <h1>Hello from github
-        MARCO MODULE TECHNOLOGIES Lahore
+       <h1>Macro Module Technologies Lahore
        </h1>
 
       </header>
