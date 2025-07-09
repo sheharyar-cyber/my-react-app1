42357feaeae2c522c951049337876b92cb888bc4
Sheharyar Ahmad <sherryjaisak@gmail.com>
2025-07-10 00:15:27 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       <h1>Macro Module Technologies Lahore
+       <h1>Macro Module Technologies wordpod/Lahore
        </h1>
 
       </header>
